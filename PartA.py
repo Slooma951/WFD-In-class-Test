514edeb361bb22eee3d21d66225e8ab6b291480d
Slooma951 <b00149614@mytudublin.ie>
2025-03-24 14:31:49 +0000

    1 child class of your main class with extra attribute

--- PartA.py
+++ PartA.py
@@ -1,3 +1,4 @@
+#A2
 class Phone:
     def __init__(self, brand, model, year, price, colour):
         self.brand = brand
@@ -6,6 +7,7 @@ class Phone:
         self.price = price
         self.colour = colour
 
+    #A3
     def display_info(self):
         print("Brand:", self.brand)
         print("Model:", self.model)
@@ -13,6 +15,7 @@ class Phone:
         print("Price:", self.price)
         print("Colour:", self.colour)
 
+    #A4
     def update_brand(self, new_brand):
         if isinstance(new_brand, str):
             self.brand = new_brand
@@ -33,10 +36,45 @@ class Phone:
         if isinstance(new_colour, str):
             self.colour = new_colour
 
+#A5
+class Smartphone(Phone):
+    def __init__(self, brand, model, year, price, colour, os, storage):
+        super().__init__(brand, model, year, price, colour)
+        self.os = os
+        self.storage = storage
+
+    #A6
+    def display_info(self):
+        super().display_info()
+        print("OS:", self.os)
+        print("Storage:", self.storage, "GB")
+
+    #A7
+    def update_os(self, new_os):
+        if isinstance(new_os, str):
+            self.os = new_os
+
+    def update_storage(self, new_storage):
+        if isinstance(new_storage, int):
+            self.storage = new_storage
+
+#A8
 phone1 = Phone("iPhone", "13", 2021, 799, "Midnight Black")
+smartphone1 = Smartphone("Iphone", "15", 2023, 899, "green", "IOS17", 256)
 
+#A9
 phone1.display_info()
-
+print("======================================")
+smartphone1.display_info()
+print("======================================")
+#A10
 phone1.update_price(600)
 phone1.update_colour("Pink")
+
+smartphone1.update_os("IOS 18")
+smartphone1.update_storage(512)
+
+
 phone1.display_info()
+print("======================================")
+smartphone1.display_info()
