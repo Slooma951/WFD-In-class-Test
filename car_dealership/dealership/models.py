d7879a84d4614bca0d94c778dacacf72896e90a7
Slooma951 <b00149614@mytudublin.ie>
2025-03-24 15:18:06 +0000

    multiple changes in partc

--- car_dealership/dealership/models.py
+++ car_dealership/dealership/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class Salesperson(models.Model):
+    salesperson_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Vehicle(models.Model):
+    vehicle_id = models.AutoField(primary_key=True)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    color = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
+
+class SalesInvoice(models.Model):
+    invoice_id = models.AutoField(primary_key=True)
+    salesperson = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    sale_date = models.DateField(auto_now_add=True)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Invoice #{self.invoice_id}"
+
+class ServiceTicket(models.Model):
+    ticket_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    service_date = models.DateField()
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Service Ticket #{self.ticket_id}"
